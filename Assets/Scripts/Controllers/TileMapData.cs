b945886b67ad6ad00b230c15dc0f3726c700c833
TIM DONLAN <TDONLAN@nextgen.com>
2015-07-23 10:59:41 -0400

    Added Tiled2Unity, tiled zone scene

---
+++ Assets/Scripts/Controllers/TileMapData.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets
+{
+
+    
+
+    public class TileMapData
+    {
+       
+
+        public List<Bounds> collisionBoundsList = new List<Bounds>();
+        public Bounds spawnBounds;
+        public List<Bounds> objectBounds = new List<Bounds>();
+
+        public List<string> objectStrings = new List<string>();
+
+        public TileMapData(GameObject tileMapGameObject)
+        {
+            loadSpawn(tileMapGameObject);
+            loadCollisionRectListFromPrefab(tileMapGameObject);
+            loadObjectBounds(tileMapGameObject);
+            loadObjectStringList();
+        }
+
+        private void loadObjectBounds(GameObject tileMapGameObject)
+        {
+            Transform objectChild = tileMapGameObject.transform.FindChild("objects");
+
+            foreach (var box in objectChild.GetComponentsInChildren<BoxCollider2D>())
+            {
+                objectBounds.Add(box.bounds);
+            }
+        }
+
+        private void loadObjectStringList()
+        {
+            objectStrings = new List<string>();
+            objectStrings.Add("You found the chest!");
+            objectStrings.Add("You are standing on the portal!");
+        }
+
+        private void loadSpawn(GameObject tileMapGameObject)
+        {
+            Transform spawnChild = tileMapGameObject.transform.FindChild("spawn");
+            spawnBounds = spawnChild.GetComponentsInChildren<BoxCollider2D>().FirstOrDefault().bounds;
+        }
+
+        private void loadCollisionRectListFromPrefab(GameObject tileMapGameObject)
+        {
+            Transform collisionChild = tileMapGameObject.transform.FindChild("collision");
+
+            foreach (var box in collisionChild.GetComponentsInChildren<BoxCollider2D>())
+            {
+                collisionBoundsList.Add(box.bounds);
+            }
+        }
+
+        public bool checkCollision(Bounds testBounds)
+        {
+            foreach (var b in collisionBoundsList)
+            {
+                if (b.Intersects(testBounds))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public string checkObjectCollision(Bounds testBounds)
+        {
+            for(int i=0;i<objectBounds.Count;i++)
+            {
+                if (objectBounds[i].Intersects(testBounds))
+                {
+                    return objectStrings[i];
+                }
+            }
+            return null;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Controllers/TileSceneControllerScript.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections.Generic;
+
+using Assets;
+
+public class TileSceneControllerScript : MonoBehaviour {
+
+    public TileMapData tileMapData;
+
+    public string debugTextString;
+    public Text debugText;
+    public Text debugText2;
+    public Text panelText;
+
+    public GameObject player;
+
+    public GameObject spritePrefab;
+    public List<GameObject> boundingSpriteList = new List<UnityEngine.GameObject>();
+
+	// Use this for initialization
+	void Start () {
+        setRefs();
+        loadTileMapData();
+        setPlayerStart();
+	}
+
+    private void setRefs()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        debugText = GameObject.FindGameObjectWithTag("debugText").GetComponent<Text>();
+        debugText2 = GameObject.FindGameObjectWithTag("debugText2").GetComponent<Text>();
+        panelText = GameObject.FindGameObjectWithTag("PanelText").GetComponent<Text>();
+
+        spritePrefab = Resources.Load<GameObject>("Prefabs/SpritePrefab");
+    }
+
+    private void loadTileMapData()
+    {
+        string outStr = "";
+        var tileMapObject = GameObject.FindGameObjectWithTag("tileMap");
+        tileMapData = new TileMapData(tileMapObject);
+        foreach (var b in tileMapData.collisionBoundsList)
+        {
+            outStr += b + "\n";
+            displayBoundingRect(b);
+        }
+        setDebugText2(outStr);
+    }
+
+    private void setPlayerStart()
+    {
+        player.transform.position = tileMapData.spawnBounds.center;
+    }
+    
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setDebugText2(string text)
+    {
+        this.debugText2.text = text;
+    }
+
+    public void setDebugText(string text)
+    {
+        this.debugText.text = text;
+    }
+
+    //display a sprite rectangle where bounds are
+    //testing
+    public void displayBoundingRect(Bounds b)
+    {
+        clearBoundingRect();
+        GameObject bSprite = Instantiate(spritePrefab);
+        bSprite.transform.position = b.center;
+        var sprite = bSprite.GetComponent<Sprite>();
+        var spriteRenderer = bSprite.GetComponent<SpriteRenderer>();
+        
+
+        boundingSpriteList.Add(bSprite);
+       
+    }
+
+    private void clearBoundingRect()
+    {
+        foreach(var b in boundingSpriteList)
+        {
+            Destroy(b);
+        }
+        boundingSpriteList.Clear();
+    }
+
+    public void checkPlayerObjectCollision(Bounds playerBounds)
+    {
+        if (tileMapData != null)
+        {
+            string objectStr = tileMapData.checkObjectCollision(playerBounds);
+            if (objectStr != null)
+            {
+                panelText.text = objectStr;
+            }
+        }
+       
+    }
+
+
+}
